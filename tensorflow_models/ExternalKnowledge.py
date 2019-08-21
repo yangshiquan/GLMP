5a52e8cafa78750c3f95bbce6dcfa925a60c49fd
Shiquan Yang <shiquan@4180L-150398-M.local>
2019-08-21 21:37:03 +1000

    remove external embedding mask

--- tensorflow_models/ExternalKnowledge.py
+++ tensorflow_models/ExternalKnowledge.py
@@ -66,7 +66,7 @@ class ExternalKnowledge(tf.keras.Model):
         self.m_story = []
         embedding_A = self.C_1(tf.reshape(story, [story_size[0], -1]))  # story: batch_size * seq_len * MEM_TOKEN_SIZE, embedding_A: batch_size * memory_size * MEM_TOKEN_SIZE * embedding_dim.
         pad_mask = self.gen_embedding_mask(tf.reshape(story, [story_size[0], -1]))
-        embedding_A = tf.multiply(embedding_A, pad_mask)
+        # embedding_A = tf.multiply(embedding_A, pad_mask)
         embedding_A = tf.reshape(embedding_A, [story_size[0], story_size[1], story_size[2], embedding_A.shape[-1]])  # embedding_A: batch_size * memory_size * MEM_TOKEN_SIZE * embedding_dim.
         embedding_A = tf.math.reduce_sum(embedding_A, 2)  # embedding_A: batch_size * memory_size * embedding_dim.
         if not args['ablationH']:
@@ -80,7 +80,7 @@ class ExternalKnowledge(tf.keras.Model):
 
         embedding_C = self.C_2(tf.reshape(story, [story_size[0], -1]))
         pad_mask = self.gen_embedding_mask(tf.reshape(story, [story_size[0], -1]))
-        embedding_C = tf.multiply(embedding_C, pad_mask)
+        # embedding_C = tf.multiply(embedding_C, pad_mask)
         embedding_C = tf.reshape(embedding_C, [story_size[0], story_size[1], story_size[2], embedding_C.shape[-1]])
         embedding_C = tf.math.reduce_sum(embedding_C, 2)  # embedding_C: batch_size * memory_size * embedding_dim.
         if not args['ablationH']:
