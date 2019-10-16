2cc41062ceeb54bf4ab977c348fd985fde15fd46
Shiquan Yang <shiquan@4180L-150398-M.local>
2019-10-16 20:58:02 +1100

    first commit, add knowledge graph and gat implementation

---
+++ tensorflow_models/GraphAttentionLayer.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import numpy as np
+
+
+class GraphAttentionLayer(tf.keras.Model):
+    def __init__(self, input_dim, output_dim, dropout, alpha, concat=True):
+        super(GraphAttentionLayer, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.dropout = dropout
+        self.alpha = alpha
+        self.concat = concat
+        self.dropout_layer = tf.keras.layers.Dropout(dropout)
+        self.W = tf.keras.layers.Dense(
+            output_dim,
+            use_bias=True,
+            kernel_initializer=tf.initializers.RandomUniform(-(1 / np.sqrt(2 * output_dim)), (1 / np.sqrt(2 * output_dim))),
+            bias_initializer=tf.initializers.RandomUniform(-(1 / np.sqrt(2 * output_dim)), (1 / np.sqrt(2 * output_dim)))
+        )  # different: bias should be explicitly assigned.
+        self.a = self.add_weight(
+            name='a',
+            shape=(self.output_dim * 2, 1),
+            initializer='zeros',
+            regularizer=None,
+            constraint=None
+        )  # self.a: (output_dim * 2) * 1
+        self.leakyrelu = tf.keras.layers.LeakyReLU(self.alpha)
+        self.elu = tf.keras.layers.ELU()
+        self.softmax = tf.keras.layers.Softmax(2)
+
+    def call(self, input, adj, training=True):  # input: batch_size * max_len * embedding_dim, adj: batch_size * max_len * max_len.
+        h = self.W(input)  # h: batch_size * max_len * output_dim.
+        batch_size, N = h.shape[0], h.shape[1]  # batch_size: batch_size, N: number of nodes in graph.
+        a_input = tf.reshape(tf.concat([tf.reshape(tf.tile(h, [1, 1, N]), [batch_size, N * N, -1]), tf.tile(h, [1, N, 1])], dim=1), [batch_size, N, -1, 2 * self.output_dim])  # a_input: batch_size * max_len * max_len * (2 * self.output_dim).
+        prob_logits = self.leakyrelu(tf.squeeze(tf.matmul(a_input, tf.tile(tf.expand_dims(tf.tile(tf.expand_dims(self.a, axis=0), [N, 1, 1]), axis=0), [batch_size, 1, 1, 1])), axis=3))  # prob_logits: batch_size * max_len * max_len.
+        prob_logits = tf.where(adj > 0, prob_logits, (-1 * np.ones_like(prob_logits) * np.inf))  # prob_logits: batch_size * max_len * max_len.
+        prob_soft = self.softmax(prob_logits)  # prob_soft: batch_size * max_len * max_len.
+        if training:
+            prob_soft = self.dropout_layer(prob_soft, training=training)  # prob_soft: batch_size * max_len * max_len.
+        h_prime = tf.matmul(prob_soft, h)  # h_prime: batch_size * max_len * output_dim.
+
+        if self.concat:
+            return self.elu(h_prime)
+        else:
+            return h_prime
---
+++ tensorflow_models/KnowledgeGraph.py
@@ -0,0 +1,127 @@
+import tensorflow as tf
+from ..utils.config import *
+import pdb
+from tensorflow.python.ops import embedding_ops
+from ..tensorflow_models.GraphAttentionLayer import GraphAttentionLayer
+
+
+class KnowledgeGraph(tf.keras.Model):
+    def __init__(self, vocab, embedding_dim, hop, nhid, nheads, alpha, dropout):
+        super(KnowledgeGraph, self).__init__()
+        self.max_hops = hop
+        self.embedding_dim = embedding_dim
+        self.vocab = vocab
+        self.nhid = nhid
+        self.nheads = nheads
+        self.alpha = alpha
+        self.dropout = dropout
+        self.dropout_layer = tf.keras.layers.Dropout(self.dropout)
+        # input embedding layer
+        self.embeddings = tf.keras.layers.Embedding(self.vocab,
+                                                    self.embedding_dim,
+                                                    embeddings_initializer=tf.initializers.RandomNormal(0.0, 0.1))  # different: no masking for pad token, pad token embedding does not equal zero, only support one hop.
+        # multi-head attention layer
+        self.attentions = [GraphAttentionLayer(embedding_dim, nhid, dropout, alpha, concat=True) for _ in range(nheads)]
+
+        # output layer
+        self.out_layer = [GraphAttentionLayer(nheads * nhid, nhid, dropout, alpha, concat=False) for _ in range(nheads)]
+
+        self.softmax = tf.keras.layers.Softmax(1)
+        self.sigmoid = tf.keras.layers.Activation('sigmoid')
+        self.elu = tf.keras.layers.ELU()
+
+    def add_lm_embedding(self, full_memory, kb_len, conv_len, hiddens):
+        output_list = []
+        for bi in range(full_memory.shape[0]):
+            stack_list = []
+            kb_aligns = tf.zeros([kb_len[bi] - 1, full_memory.shape[2]])
+            null_aligns = tf.zeros([1, full_memory.shape[2]])
+            # print('kb_len:', kb_len[bi].numpy())
+            if (kb_len[bi] - 1).numpy().item() != 0:
+                kb_aligns_slices = tf.split(kb_aligns, num_or_size_splits=kb_aligns.shape[0], axis=0)
+                for tensor in kb_aligns_slices:
+                    stack_list.append(tensor)
+            # pdb.set_trace()
+            hiddens_slices = tf.split(hiddens[bi, :conv_len[bi], :], num_or_size_splits=conv_len[bi].numpy().item(),
+                                      axis=0)
+            # print('hiddens len:', hiddens[bi].shape[0])
+            for tensor in hiddens_slices:
+                stack_list.append(tensor)
+            # pdb.set_trace()
+            stack_list.append(null_aligns)
+            pad_len = full_memory.shape[1] - kb_len[bi] - conv_len[bi]
+            # pdb.set_trace()
+            if pad_len.numpy().item() != 0:
+                pad_aligns = tf.zeros([pad_len, full_memory.shape[2]])
+                pad_aligns_slice = tf.split(pad_aligns, num_or_size_splits=pad_aligns.shape[0], axis=0)
+                for tensor in pad_aligns_slice:
+                    stack_list.append(tensor)
+                # pdb.set_trace()
+            # print('stack_list length:', len(stack_list))
+            add_tensor = tf.squeeze(tf.stack(stack_list, axis=0), 1)
+            updated_full_memory = full_memory[bi] + add_tensor
+            output_list.append(updated_full_memory)
+        output = tf.stack(output_list, axis=0)
+        return output
+
+    def gen_embedding_mask(self, input):
+        raw_mask_array = [[1.0]] * PAD_token + [[0.0]] + [[1.0]] * (self.vocab - PAD_token - 1)
+        mask = embedding_ops.embedding_lookup(raw_mask_array, tf.cast(input, dtype=tf.int32))
+        ret_mask = tf.tile(tf.expand_dims(mask, 2), [1, 1, self.embedding_dim])
+        return ret_mask
+
+    def load_graph(self, story, kb_len, conv_len, hidden, dh_outputs, adj, training=True):
+        u = [hidden]  # different: hidden without squeeze(0), hidden: batch_size * embedding_size.
+        story_size = story.shape
+        self.m_story = []
+
+        # transform one-hot to embeddings
+        embedding_A = self.embeddings(tf.reshape(story, [story_size[0], -1]))  # story: batch_size * seq_len * MEM_TOKEN_SIZE, embedding_A: batch_size * memory_size * MEM_TOKEN_SIZE * embedding_dim.
+        pad_mask = self.gen_embedding_mask(tf.reshape(story, [story_size[0], -1]))
+        # mask pad token embeddings
+        embedding_A = tf.multiply(embedding_A, pad_mask)
+        embedding_A = tf.reshape(embedding_A, [story_size[0], story_size[1], story_size[2], embedding_A.shape[-1]])  # embedding_A: batch_size * memory_size * MEM_TOKEN_SIZE * embedding_dim.
+        embedding_A = tf.math.reduce_sum(embedding_A, 2)  # embedding_A: batch_size * memory_size * embedding_dim.
+        if training:
+            embedding_A = self.dropout_layer(embedding_A, training=training)
+
+        # First Layer, GraphAttentionLayer to update word embeddings
+        embedding_A = tf.concat([att(embedding_A, adj, training) for att in self.attentions], axis=2)  # embedding_A: batch_size * memory_size * (nhead * embedding_dim)
+        # Second Layer (final layer), apply output layer
+        embedding_A = [head(embedding_A, adj, training) for head in self.out_layer]
+        # average multi-head embeddings
+        embedding_A = tf.reduce_sum(tf.stack(embedding_A, axis=0), axis=0) / tf.cast(self.nheads, dtype=tf.float32)  # embedding_A: batch_size * memory_size * embedding_dim.
+        # apply non-linearity
+        embedding_A = self.elu(embedding_A)  # embedding_A: batch_size * memory_size * embedding_dim.
+
+        if not args['ablationH']:
+            embedding_A = self.add_lm_embedding(embedding_A, kb_len, conv_len, dh_outputs)
+
+        u_temp = tf.tile(tf.expand_dims(u[-1], 1), [1, embedding_A.shape[1], 1])  # u_temp: batch_size * memory_size * embedding_dim.
+        prob_logits = tf.math.reduce_sum((embedding_A * u_temp), 2)  # prob_logits: batch_size * memory_size
+
+        self.m_story.append(embedding_A)
+
+        return self.sigmoid(prob_logits), u[-1], prob_logits
+
+    def call(self, query_vector, global_pointer, training=True):
+        u = [query_vector]  # query_vector: batch_size * embedding_dim.
+
+        embed_A = self.m_story[0]  # embed_A: batch_size * memory_size * embedding_dim.
+        if not args['ablationG']:
+            embed_A = embed_A * tf.tile(tf.expand_dims(global_pointer, 2), [1, 1, embed_A.shape[2]])
+
+        u_temp = tf.tile(tf.expand_dims(u[-1], 1), [1, embed_A.shape[1], 1])  # u_temp: batch_size * memory_size * embedding_dim.
+        prob_logits = tf.math.reduce_sum((embed_A * u_temp), 2)  # prob_logits: batch_size * memory_size.
+        prob_soft = self.softmax(prob_logits)  # prob_soft: batch_size * memory_size.
+
+        return prob_soft, prob_logits
+
+
+class AttrProxy(object):
+    def __init__(self, module, prefix):
+        self.module = module
+        self.prefix = prefix
+
+    def __getitem__(self, i):
+        return getattr(self.module, self.prefix + str(i))
