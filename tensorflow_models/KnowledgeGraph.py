96c8b553a75586047ead94dd2f0d600950922ec3
Shiquan Yang <shiquan@4180L-150398-M.local>
2019-10-19 18:41:08 +1100

    mimic baseline memoty

--- tensorflow_models/KnowledgeGraph.py
+++ tensorflow_models/KnowledgeGraph.py
@@ -98,17 +98,17 @@ class KnowledgeGraph(tf.keras.Model):
         if training:
             embedding_A = self.dropout_layer(embedding_A, training=training)
 
-        # adj = self.update_pad_token_adj(adj, kb_len, conv_len)
-        # # First Layer, GraphAttentionLayer to update word embeddings
-        # embedding_A = tf.concat([att(embedding_A, adj, training) for att in self.attentions], axis=2)  # embedding_A: batch_size * memory_size * (nhead * embedding_dim)
-        # # Second Layer (final layer), apply output layer
-        # embedding_A = [head(embedding_A, adj, training) for head in self.out_layer]
-        # # average multi-head embeddings
-        # embedding_A = tf.reduce_sum(tf.stack(embedding_A, axis=0), axis=0) / tf.cast(self.nheads, dtype=tf.float32)  # embedding_A: batch_size * memory_size * embedding_dim.
-        # # apply non-linearity
-        # # embedding_A = self.elu(embedding_A)  # embedding_A: batch_size * memory_size * embedding_dim.
-        # # add for mimic memory
-        # embedding_A = tf.identity(embedding_A)  # embedding_A: batch_size * memory_size * embedding_dim.
+        adj = self.update_pad_token_adj(adj, kb_len, conv_len)
+        # First Layer, GraphAttentionLayer to update word embeddings
+        embedding_A = tf.concat([att(embedding_A, adj, training) for att in self.attentions], axis=2)  # embedding_A: batch_size * memory_size * (nhead * embedding_dim)
+        # Second Layer (final layer), apply output layer
+        embedding_A = [head(embedding_A, adj, training) for head in self.out_layer]
+        # average multi-head embeddings
+        embedding_A = tf.reduce_sum(tf.stack(embedding_A, axis=0), axis=0) / tf.cast(self.nheads, dtype=tf.float32)  # embedding_A: batch_size * memory_size * embedding_dim.
+        # apply non-linearity
+        # embedding_A = self.elu(embedding_A)  # embedding_A: batch_size * memory_size * embedding_dim.
+        # add for mimic memory
+        embedding_A = tf.identity(embedding_A)  # embedding_A: batch_size * memory_size * embedding_dim.
 
         if not args['ablationH']:
             embedding_A = self.add_lm_embedding(embedding_A, kb_len, conv_len, dh_outputs)
