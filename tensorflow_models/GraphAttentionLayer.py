c7ca6bbcb29aea160823819c43c8b866d79a204c
Shiquan Yang <shiquan@4180L-150398-M.local>
2019-10-24 18:56:35 +1100

    optimize code

--- tensorflow_models/GraphAttentionLayer.py
+++ tensorflow_models/GraphAttentionLayer.py
@@ -20,13 +20,27 @@ class GraphAttentionLayer(tf.keras.Model):
             kernel_regularizer=tf.keras.regularizers.l2(0.001),
             bias_regularizer=tf.keras.regularizers.l2(0.001)
         )  # different: bias should be explicitly assigned.
-        self.a = self.add_weight(
-            name='a',
-            shape=(self.output_dim * 2, 1),
+        # self.a = self.add_weight(
+        #     name='a',
+        #     shape=(self.output_dim * 2, 1),
+        #     initializer='glorot_uniform',
+        #     regularizer=None,
+        #     constraint=None
+        # )  # self.a: (output_dim * 2) * 1
+        self.a1 = self.add_weight(
+            name='a1',
+            shape=(self.output_dim, 1),
             initializer='glorot_uniform',
             regularizer=None,
             constraint=None
-        )  # self.a: (output_dim * 2) * 1
+        )  # self.a1: (output_dim, 1)
+        self.a2 = self.add_weight(
+            name='a2',
+            shape=(self.output_dim, 1),
+            initializer='glorot_uniform',
+            regularizer=None,
+            constraint=None
+        )  # self.a2: (output_dim, 1)
         self.leakyrelu = tf.keras.layers.LeakyReLU(self.alpha)
         self.elu = tf.keras.layers.ELU()
         self.softmax = tf.keras.layers.Softmax(2)
@@ -37,8 +51,11 @@ class GraphAttentionLayer(tf.keras.Model):
         # pdb.set_trace()
         # h = tf.identity(input)
         batch_size, N = h.shape[0], h.shape[1]  # batch_size: batch_size, N: number of nodes in graph.
-        a_input = tf.reshape(tf.concat([tf.reshape(tf.tile(h, [1, 1, N]), [batch_size, N * N, -1]), tf.tile(h, [1, N, 1])], axis=2), [batch_size, N, -1, 2 * self.output_dim])  # a_input: batch_size * max_len * max_len * (2 * self.output_dim).
-        prob_logits = self.leakyrelu(tf.squeeze(tf.matmul(a_input, tf.tile(tf.expand_dims(tf.tile(tf.expand_dims(self.a, axis=0), [N, 1, 1]), axis=0), [batch_size, 1, 1, 1])), axis=3))  # prob_logits: batch_size * max_len * max_len.
+        # a_input = tf.reshape(tf.concat([tf.reshape(tf.tile(h, [1, 1, N]), [batch_size, N * N, -1]), tf.tile(h, [1, N, 1])], axis=2), [batch_size, N, -1, 2 * self.output_dim])  # a_input: batch_size * max_len * max_len * (2 * self.output_dim).
+        # prob_logits = self.leakyrelu(tf.squeeze(tf.matmul(a_input, tf.tile(tf.expand_dims(tf.tile(tf.expand_dims(self.a, axis=0), [N, 1, 1]), axis=0), [batch_size, 1, 1, 1])), axis=3))  # prob_logits: batch_size * max_len * max_len.
+        f_1 = h @ self.a1
+        f_2 = h @ self.a2
+        prob_logits = self.leakyrelu(f_1 + tf.transpose(f_2, [1, 0]))
         prob_logits = tf.where(adj > 0, prob_logits, (-1 * np.ones_like(prob_logits) * np.inf))  # prob_logits: batch_size * max_len * max_len.
         prob_soft = self.softmax(prob_logits)  # prob_soft: batch_size * max_len * max_len.
         # comment for mimic memory
