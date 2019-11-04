cd2042e9829186f3d22fec17d55e62b069e44643
Shiquan Yang <shiquan@4180L-150398-M.local>
2019-11-04 13:13:44 +1100

    ad

--- models/GLMP.py
+++ models/GLMP.py
@@ -37,13 +37,15 @@ class GLMP(nn.Module):
                 print("MODEL {} LOADED".format(str(path)))
                 self.encoder = torch.load(str(path)+'/enc.th')
                 self.extKnow = torch.load(str(path)+'/enc_kb.th')
-                self.extKnow = torch.load(str(path) + '/enc_kb4h.th')
+                # self.extKnow4Head = torch.load(str(path) + '/enc_kb4h.th')
+                self.extKnow4Head = torch.load('/home/student.unimelb.edu.au/shiquan/pytorch_graph_memory_with_head_pointer_change_data_label/GLMP/save/GLMP-KVR/HDD128BSZ32DR0.2L3lr0.001ENTF1-0.5380enc_kb4h.th')
                 self.decoder = torch.load(str(path)+'/dec.th')
             else:
                 print("MODEL {} LOADED".format(str(path)))
                 self.encoder = torch.load(str(path)+'/enc.th',lambda storage, loc: storage)
                 self.extKnow = torch.load(str(path)+'/enc_kb.th',lambda storage, loc: storage)
-                self.extKnow4Head = torch.load(str(path)+'/enc_kb4h.th',lambda storage, loc: storage)
+                # self.extKnow4Head = torch.load(str(path)+'/enc_kb4h.th',lambda storage, loc: storage)
+                self.extKnow4Head = torch.load('/home/student.unimelb.edu.au/shiquan/pytorch_graph_memory_with_head_pointer_change_data_label/GLMP/save/GLMP-KVR/HDD128BSZ32DR0.2L3lr0.001ENTF1-0.5380enc_kb4h.th',lambda storage, loc: storage)
                 self.decoder = torch.load(str(path)+'/dec.th',lambda storage, loc: storage)
         else:
             self.encoder = ContextRNN(lang.n_words, hidden_size, dropout)
