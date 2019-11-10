8708ebfc511c76fe96705491b95c29d54da03228
Shiquan Yang <shiquan@4180L-150398-M.local>
2019-11-10 16:10:44 +1100

    change rnn initializer parameters to (0.0, 0.01)

--- tensorflow_models/Libraries/RNN.py
+++ tensorflow_models/Libraries/RNN.py
@@ -46,11 +46,11 @@ class RNN(tf.keras.Model):
                                  shared_emb,
                                  recurrent_size,
                                  # kernel_initializer=tf.initializers.RandomUniform(-(1/np.sqrt(units)),(1/np.sqrt(units))),
-                                 kernel_initializer=tf.initializers.RandomNormal(0.0, 0.1),
+                                 kernel_initializer=tf.initializers.RandomNormal(0.0, 0.01),
                                  # recurrent_initializer=tf.initializers.RandomUniform(-(1/np.sqrt(units)),(1/np.sqrt(units))),
-                                 recurrent_initializer=tf.initializers.RandomNormal(0.0, 0.1),
+                                 recurrent_initializer=tf.initializers.RandomNormal(0.0, 0.01),
                                  # bias_initializer=tf.initializers.RandomUniform(-(1/np.sqrt(units)),(1/np.sqrt(units)))),
-                                 bias_initializer = tf.initializers.RandomNormal(0.0, 0.1))
+                                 bias_initializer = tf.initializers.RandomNormal(0.0, 0.01))
 
     def call(self,
              inputs,  # inputs: batch_size*max_len*embedding_dim
