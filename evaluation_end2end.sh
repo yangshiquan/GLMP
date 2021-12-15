27e00b05c848d445e27fe032cd4f512a2fd7c754
Shiquan Yang <shiquan@4180L-150398-M.local>
2021-12-15 22:03:59 +1100

    commit

--- evaluation_end2end.sh
+++ evaluation_end2end.sh
@@ -8,10 +8,10 @@ export ent_pred_model_path=$2
 python generate_responses.py -path=${resp_gen_model_path}
 
 # predict per-timestep entities using generated responses
-# python predict_entities.py -path=${ent_pred_model_path}
+python predict_entities.py -path=${ent_pred_model_path}
 
 # lexicalize the generated responses using predicted entities
-# python lexicalize_generated_responses_using_predicted_entities.py
+python lexicalize_generated_responses_using_predicted_entities.py
 
 # calculate final metrics using lexicalized responses and gold responses
-# python evaluate_lexicalized_responses.py
+python evaluate_lexicalized_responses.py
